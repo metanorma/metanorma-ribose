74d91400822933f64f34250c811d30fda789dc3d
Ronald Tse <ronald.tse@ribose.com>
2019-01-02 02:45:23 +0800

    Really check for unpublished

--- lib/isodoc/rsd/metadata.rb
+++ lib/isodoc/rsd/metadata.rb
@@ -39,7 +39,7 @@ module IsoDoc
       end
 
       def unpublished(status)
-        %w(published withdrawn).include? status.downcase
+        !%w(published withdrawn).include? status.downcase
       end
 
       def version(isoxml, _out)
