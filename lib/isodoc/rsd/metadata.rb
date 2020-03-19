c7549e981770500b96693908a10c1b4d58a3ada6
Nick Nicholas <opoudjis@gmail.com>
2020-03-19 22:29:09 +1100

    RSD restyling: https://github.com/metanorma/metanorma-rsd/issues/39

--- lib/isodoc/rsd/metadata.rb
+++ lib/isodoc/rsd/metadata.rb
@@ -13,10 +13,16 @@ module IsoDoc
         set(:security, security.text)
       end
 
+      def recipient(isoxml, _out)
+        recipient = isoxml.at(ns("//bibdata/ext/recipient")) || return
+        set(:recipient, recipient.text)
+      end
+
       def version(isoxml, _out)
         super
         revdate = get[:revdate]
         set(:revdate_monthyear, monthyr(revdate))
+        set(:revdate_MMMddyyyy, MMMddyyyy(revdate))
       end
 
       MONTHS = {
@@ -39,6 +45,19 @@ module IsoDoc
         return isodate unless m && m[:yr] && m[:mo]
         return "#{MONTHS[m[:mo].to_sym]} #{m[:yr]}"
       end
+
+      def MMMddyyyy(isodate)
+        return nil if isodate.nil?
+        arr = isodate.split("-")
+        date = if arr.size == 1 and (/^\d+$/.match isodate)
+                 Date.new(*arr.map(&:to_i)).strftime("%Y")
+               elsif arr.size == 2
+                 Date.new(*arr.map(&:to_i)).strftime("%B %Y")
+               else
+                 Date.parse(isodate).strftime("%B %d, %Y")
+               end
+      end
+
     end
   end
 end
