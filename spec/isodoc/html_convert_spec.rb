5b41a7aba6ba30cffbd5765660827ae87a8d31a2
Nick Nicholas <opoudjis@gmail.com>
2025-04-30 20:43:09 +1000

    rspec

--- spec/isodoc/html_convert_spec.rb
+++ spec/isodoc/html_convert_spec.rb
@@ -996,7 +996,7 @@ RSpec.describe IsoDoc::Ribose do
     INPUT
 
     presxml = <<~INPUT
-       <ogc-standard xmlns="https://standards.opengeospatial.org/document" type="presentation">
+     <ogc-standard xmlns="https://standards.opengeospatial.org/document" type="presentation">
           <preface>
              <clause type="toc" id="_" displayorder="1">
                 <fmt-title depth="1">Contents</fmt-title>
@@ -1081,7 +1081,7 @@ RSpec.describe IsoDoc::Ribose do
                          <semx element="deprecates" source="_">Term2D</semx>
                       </p>
                    </fmt-deprecates>
-                   <termsource status="modified" id="_">
+                   <source status="modified" id="_">
                       <origin bibitemid="ISO7301" type="inline" citeas="ISO 7301:2011">
                          <locality type="clause">
                             <referenceFrom>3.1</referenceFrom>
@@ -1090,11 +1090,11 @@ RSpec.describe IsoDoc::Ribose do
                       <modification id="_">
                          <p original-id="_e73a417d-ad39-417d-a4c8-20e4e2529489">The term "cargo rice" is shown as deprecated, and Note 1 to entry is not included here</p>
                       </modification>
-                   </termsource>
+                   </source>
                    <fmt-termsource status="modified">
                       <strong>SOURCE</strong>
                       :
-                      <semx element="termsource" source="_">
+                      <semx element="source" source="_">
                          <origin bibitemid="ISO7301" type="inline" citeas="ISO 7301:2011" id="_">
                             <locality type="clause">
                                <referenceFrom>3.1</referenceFrom>
